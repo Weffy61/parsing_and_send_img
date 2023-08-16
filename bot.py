bcc77a3d71b3b5c4c411f9ff6bdad446acbf4610
Weffy61 <weffy@mail.ru>
2023-08-17 00:39:20 +0300

    publish photo to Telegram channel

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import telegram
 from environs import Env
 
+
 env = Env()
 env.read_env()
-
 bot = telegram.Bot(token=env('TELEGRAM_API_TOKEN'))
-bot.send_message(text='Hello!', chat_id='-1001471388120')
+
+
+with open('images/nasa_epic/nasa_epic_0.png', 'rb') as image:
+    bot.send_photo(chat_id='-1001471388120', photo=image)
